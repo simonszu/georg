f58f172c516901ecfaf56537f07fb993a25642f5
Simon Szustkowski <mail@simonszu.de>
2023-06-12 19:15:56 +0200

    Better working minimal version

--- bot.py
+++ bot.py
@@ -3,10 +3,21 @@ import discord
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
   print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
 
 client.run(TOKEN)
